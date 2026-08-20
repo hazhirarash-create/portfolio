cfa298b6393a1ce40b6b8d8e7426584a69019f43
Arash <arash@example.com>
2026-08-20 11:36:30 +0330

    hashing_password

--- security/password.py
+++ security/password.py
@@ -1,4 +1,6 @@
 from pwdlib import PasswordHash
+import hashlib
+import requests
 
 
 password_hash = PasswordHash.recommended()
@@ -6,6 +8,39 @@ password_hash = PasswordHash.recommended()
 DUMMY_PASSWORD_HASH = password_hash.hash(
     "dummy-password-for-timing"
 )
+
+def is_pwned_password(password: str) -> bool:
+
+    password_hash = hashlib.sha1(
+        password.encode("utf-8")
+    ).hexdigest().upper()
+
+    prefix = password_hash[:5]
+    suffix = password_hash[5:]
+
+    url = (
+        "https://api.pwnedpasswords.com/range/"
+        f"{prefix}"
+    )
+
+    try:
+        response = requests.get(
+            url,
+            timeout=3
+        )
+
+        response.raise_for_status()
+
+        for line in response.text.splitlines():
+            returned_suffix, _count = line.split(":", 1)
+
+            if returned_suffix == suffix:
+                return True
+
+        return False
+
+    except requests.RequestException:
+        return False
 
 
 def hash_password(password: str) -> str:
